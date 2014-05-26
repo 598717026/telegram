3b8f8aa2eabbb291b4b704e28fe4b35c1107c1f3
598717026 <598717026@qq.com>
2014-05-27 00:10:54 +0800

    add fft

--- telegram/src/com/example/telegram/MainActivity.java
+++ telegram/src/com/example/telegram/MainActivity.java
@@ -17,87 +17,87 @@ import android.widget.Button;
 public class MainActivity extends Activity {
 
 	private Button mButton;
-	boolean mTouchDown = false;
+	private boolean mTouchDown = false;
+	public MyAudioTrack maudiotrack;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		ControlThread();
 		
-		
-		mButton = (Button) findViewById(R.id.buttonpcm);
+		/*mButton = (Button) findViewById(R.id.buttonpcm);
 		mButton.setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-//				final Runnable audioRecRun = new Runnable() {
-//					public void run() {
-						MyAudioTrack maudiotrack = new MyAudioTrack(44100, AudioFormat.CHANNEL_CONFIGURATION_STEREO, AudioFormat.ENCODING_PCM_16BIT);
-						maudiotrack.init();
-						short [] data = new short[44100];
-						
-						byte [] buffer = null; 
-						 int length = 0;
-						String fileName = "/sdcard/radio.pcm";
+				// final Runnable audioRecRun = new Runnable() {
+				// public void run() {
+				MyAudioTrack maudiotrack = new MyAudioTrack(44100,
+						AudioFormat.CHANNEL_CONFIGURATION_STEREO,
+						AudioFormat.ENCODING_PCM_16BIT);
+				maudiotrack.init();
+				short[] data = new short[44100];
 
-						//也可以用String fileName = "mnt/sdcard/Y.txt";
+				byte[] buffer = null;
+				int length = 0;
+				String fileName = "/sdcard/radio.pcm";
 
-						String res="";     
+				// 也可以用String fileName = "mnt/sdcard/Y.txt";
 
-						try{ 
+				String res = "";
 
-						FileInputStream fin = new FileInputStream(fileName);
+				try {
 
-						//FileInputStream fin = openFileInput(fileName);  
+					FileInputStream fin = new FileInputStream(fileName);
 
-						//用这个就不行了，必须用FileInputStream
+					// FileInputStream fin = openFileInput(fileName);
 
-						    length = fin.available(); 
+					// 用这个就不行了，必须用FileInputStream
 
-						    buffer = new byte[length]; 
+					length = fin.available();
 
-						    fin.read(buffer);     
+					buffer = new byte[length];
 
+					fin.read(buffer);
 
-						    fin.close();     
+					fin.close();
 
-						    }catch(Exception e){ 
+				} catch (Exception e) {
 
-						           e.printStackTrace(); 
+					e.printStackTrace();
 
-						} 
-						maudiotrack.playAudioTrack(buffer, 0, length);
-						Log.i("maudiotrack", "playAudioTrack(data, 0, 5120)");
-						new AlertDialog.Builder(MainActivity.this) 
-				          .setMessage("这是第二个提示") 
-				          .setPositiveButton("确定", 
-				                         new DialogInterface.OnClickListener(){ 
-				                                 public void onClick(DialogInterface dialoginterface, int i){ 
-				                                     //按钮事件 
-				                                  } 
-				                          }) 
-				          .show();
+				}
+				maudiotrack.playAudioTrack(buffer, 0, length);
+				Log.i("maudiotrack", "playAudioTrack(data, 0, 5120)");
+				new AlertDialog.Builder(MainActivity.this)
+						.setMessage("这是第二个提示")
+						.setPositiveButton("确定",
+								new DialogInterface.OnClickListener() {
+									public void onClick(
+											DialogInterface dialoginterface,
+											int i) {
+										// 按钮事件
+									}
+								}).show();
 
-
-						maudiotrack.release();
-						data = null;
-//					}
-//				};
+				maudiotrack.release();
+				data = null;
+				// }
+				// };
 			}
-		});
+		});*/
 	}
-	
+
 	@Override
-	public boolean onTouchEvent(MotionEvent event)
-	{
+	public boolean onTouchEvent(MotionEvent event) {
 		int p = event.findPointerIndex(0);
 		float x = event.getX(p);
 		float y = event.getY(p);
-		
-		switch (event.getAction())
-		{
+
+		switch (event.getAction()) {
 		case MotionEvent.ACTION_DOWN:
 			mTouchDown = true;
 			break;
@@ -109,30 +109,65 @@ public class MainActivity extends Activity {
 		}
 		return super.onTouchEvent(event);
 	}
-	
-	public void ControlThread()
-	{
+
+	public void ControlThread() {
+		maudiotrack = new MyAudioTrack(44100,
+				AudioFormat.CHANNEL_CONFIGURATION_STEREO,
+				AudioFormat.ENCODING_PCM_16BIT);
+		maudiotrack.init();
+		
+		short[] buffer = new short[44100 * 2];
+		Complex[] x = new Complex[44100];
+
+		for (int i  = 0;i < 44100;i++)
+		{
+			x[i] = new Complex(0, 0);
+		}
+		
+
+		x[840] = new Complex(1, 0);
+
+		Complex[] y = FFT.ifft(x);
+		
+		for (int i  = 0;i < 44100;i++)
+		{
+			buffer[i * 2] = (short) y[i].re();
+			buffer[i * 2 + 1] = (short) y[i].re();
+		}
+
+		for (int i = 0;i < 100;i++)
+		{
+			
+			Log.i("maudiotrack", "buff:" + i + "," + buffer[i]);
+		}
+		
 		final Runnable audioRun = new Runnable() {
 			public void run() {
 				while (true) {
 
-					if (false == mTouchDown)
-					{
-						
+					if (true == mTouchDown) {
+
+						byte[] buffer = null;
+						int length = 0;
+						maudiotrack.playAudioTrack(buffer , 0, length );
+						Log.i("maudiotrack", "playAudioTrack(data, 0, 5120)");
+
+						buffer = null;
+
+					} else {
+
 					}
-					else
-					{
-						
+
+					try {
+						Thread.sleep(100);
+					} catch (Exception e) {
+						e.printStackTrace();
 					}
-					
-		            try {  
-		                Thread.sleep(100);  
-		            } catch (Exception e) {  
-		                e.printStackTrace();  
-		            }  
-		        }  
+				}
 			}
-			};
+		};
+
+		maudiotrack.release();
 	}
 
 }
